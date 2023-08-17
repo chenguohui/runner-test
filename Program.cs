37d68ad3d2493c25c754826d100f7681d4c7cacf
Chen Guohui <chenguohui@loongson.cn>
2023-08-17 10:48:52 +0800

    Add VM-ubuntu node

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Running on github");
+Console.WriteLine("Add VM-ubuntu node");
