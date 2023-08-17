8f76ab2d95326f49edb338095eddc31b56866d6b
Chen Guohui <chenguohui@loongson.cn>
2023-08-17 14:18:15 +0800

    Add two runners

--- Program.cs
+++ Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Running on github");
 Console.WriteLine("Add VM-ubuntu node");
 Console.WriteLine("Add self-hosted");
+Console.WriteLine("Add two runners");
